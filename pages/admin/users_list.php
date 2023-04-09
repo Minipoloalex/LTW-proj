6ad2d9d406fc50148243fe81f1c0947093543e17
Sofia <up202108676@up.pt>
2023-04-09 19:44:46 +0100

    users list

--- pages/admin/users_list.php
+++ pages/admin/users_list.php
@@ -1,4 +1,21 @@
 <?php
-?>
+declare(strict_types=1);
+require_once(__DIR__ . '/../../database/connection.db.php');
+require_once(__DIR__ . '/../../utils/session.php');
+require_once(__DIR__ . '/../../database/client.class.php');
+require_once(__DIR__ . '/../../database/department.class.php');
+require_once(__DIR__ . '/../../database/ticket.class.php');
+require_once(__DIR__ . '/../../database/agent.class.php');
+require_once(__DIR__ . '/../../templates/common.tpl.php');
+require_once(__DIR__ . '/../../templates/departments.tpl.php');
 
+$count = 100; /* limite de users na lista */
+$db = getDatabaseConnection();
+$users = Client::getClients($db, $count);
+
+output_header();
+drawUsersTable($users, $db);
+output_footer();
+
+?>
 <!--- users list -> nome, id, username, tipo de user (?), nr tickets --->
--- templates/users.tpl.php
+++ templates/users.tpl.php
@@ -1,21 +1,49 @@
 <?php 
 declare(strict_types = 1);
 
+require_once(__DIR__ . '/../database/department.class.php');
+require_once(__DIR__ . '/../database/ticket.class.php');
+require_once(__DIR__ . '/../database/agent.class.php');
+require_once(__DIR__ . '/../database/client.class.php');
+
 ?>
 
-<?php function output_single_user(Client $user) { ?>
-    <li>
-
-    </li>
+<!--- confirmar o GET BY AGENT e BY USER (ver se interpretei isso bem), como fazer o user type... --->
+<?php function drawUser(Client $user, PDO $db) { ?>
+    <?php $nr_tickets_created = count(Ticket::getByUser($db, $user->id))?>
+    <?php $nr_tickets_assigned = count(Ticket::getByAgent($db, $user->id))?>
+    
+    <tr>
+        <td><?=$user->name?></td>
+        <td><?=$user->id?></td>
+        <td><?=$user->username?></td>
+        <td><?='USERTYPE'?></td>
+        <td><?=$nr_tickets_created?></td>
+        <td><?=$nr_tickets_assigned?></td>
 <?php } ?>
 
-<?php function output_users(array $users) { ?> 
+<?php function drawUsersTable(array $users, PDO $db) { ?> 
     <section id="users">
-        <ul>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>User ID</th>
+                <th>Username</th>
+                <th>User type</th>
+                <th>Nr Tickets (created)</th>
+                <th>Nr Tickets (assigned)</th>
+                <th>Department</th>
+            </tr>
+        </thead>
+
+        <tbody>
             <?php
-            foreach($users as $user) output_single_user($user);
+            foreach($users as $user) {
+                drawUser($user, $db);
+            }
             ?>
-            
-        </ul>
+        </tbody>
+    </table>
     </section>
 <?php } ?>
