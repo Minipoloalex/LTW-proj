a46030065e4c72e1c4fae0477136ed90b772b244
Félix Martins <felix.marcial@hotmail.com>
2023-04-25 15:38:07 +0100

    Started working on changing users type

--- database/connection.db.php
+++ database/connection.db.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 function getDatabaseConnection(): PDO
 {
     $db = new PDO('sqlite:' . __DIR__ . '/../database/database.db');
+    
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->exec('PRAGMA foreign_keys = ON;');
 
     return $db;
 }
