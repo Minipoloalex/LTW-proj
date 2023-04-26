c948545e9440e638e5a4c798eb00c760a0f5b5b0
g_monteiro <gbmonteiro24@gmail.com>
2023-04-26 20:10:33 +0100

    started implementing cards

--- pages/tickets.php
+++ pages/tickets.php
@@ -10,44 +10,7 @@ $db = getDatabaseConnection();
 $filters = Ticket::getFilters($db);
 output_header();
 drawFilterMenu($filters);
-// $tickets = Ticket::filter($db, [], ['"high"'], [], [], []);
-// var_dump($tickets);
 $tickets = Ticket::getTickets($db);
 drawTicketsTable($tickets, 'All Tickets');
 output_footer();
-// handleRequest($db);
-/*
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  echo ('hello');
-  // Handle AJAX request
-  $filterValues = json_decode(file_get_contents('php://input'), true);
-  var_dump($filterValues);
-  $agents = $filters['agents'];
-  $departments = $filters['departments'];
-  $hastags = $filters['hastags'];
-  $priorities = $filters['priorities'];
-  $status = $filters['status'];
-  
-  if (empty($agents) && empty($departments) && empty($hastags) && empty($priorities) && empty($status)) {
-      // Draw all tickets table if no filters are selected
-      $tickets = Ticket::getTickets($db);
-      drawTicketsTable($tickets, 'All Tickets');
-      output_footer();
-  } else {
-      // Get filtered tickets
-      // $tickets = Ticket::getTickets($db);
-      $tickets = Ticket::filter($db, $status, $priorities, $hastags, $agents, $departments);
-
-      // Draw filtered tickets table
-      drawTicketsTable($tickets, 'Filtered Tickets');
-      output_footer();
-  }
-} else {
-  echo ('bye');
-  // Draw all tickets table
-  $tickets = Ticket::getTickets($db);
-  drawTicketsTable($tickets, 'All Tickets');
-  output_footer();
-}*/
-
 ?>
