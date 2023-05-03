3a1b464e1e6dbbc53d1045ca1cd4732c05536dfa
Sofia <up202108676@up.pt>
2023-05-03 19:54:17 +0100

    missed one line in html while merge

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -16,8 +16,8 @@ require_once(__DIR__ . '/../utils/session.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="../javascript/utils.js" defer></script>
-    <script src="../javascript/add_message.js" defer></script>
     <script src="../javascript/nav.js" defer></script>
+    <script src="../javascript/add_message.js" defer></script>
     <script src="../javascript/filter.js" defer></script>
     <script src="../javascript/edit_profile.js" defer></script>
     <script src="../javascript/update_ticket.js" defer></script>
@@ -25,8 +25,9 @@ require_once(__DIR__ . '/../utils/session.php');
     <!-- <script src="../javascript/table_pagination.js" defer></script> -->
     <!-- <script src="../javascript/manage_FAQ.js" defer></script> -->
     <script src="../javascript/manage_FAQ2.js" defer></script>
-    <script src="../javascript/cards.js" defer></script>
+    <script src="../javascript/cards.js" defer></script>  
     <script src="../javascript/hashtag_autocomplete.js" defer></script>
+
 
     <link rel="stylesheet" href="../css/nav.css">
     <link rel="stylesheet" href="../css/individual_ticket.css">
@@ -48,25 +49,28 @@ require_once(__DIR__ . '/../utils/session.php');
       integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
       crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!---FAQ icons--->
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-</head>
+    <!---FAQ icons---> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    </head>
 
-<body>
-    <nav class="navbar">
+  <body>
+    <!-- <header>
+        <h1><a href="main_page.php">Trouble Solver</a></h1>
+      </header> -->
+    <nav class="navbar" >
       <ul class="navbar-nav">
+
+        <!-- <li class="logo">Trouble Solver</li> -->
         <li class="logo"><a href="../pages/main_page.php"><img src="../css/images/logo.png" alt="logo"></a></li>
         <li class="nav-item"><a href="../pages/profile.php" class="nav-link"><span
               class="material-symbols-outlined">person</span><span class="link-text">Profile</span></a></li>
-        <li class="nav-item"><span class="nav-submenu-wrapper" id="subWrapper" ><span
-              class="nav-submenu-header"><span class="material-symbols-outlined">confirmation_number</span><span
-                class="link-text" id="subHeaderTitle">Tickets</span></span>
+        <li class="nav-item"><span class="nav-submenu-wrapper" id="subWrapper" onclick="toggleSubMenu()"><span class="nav-submenu-header"><span
+              class="material-symbols-outlined">confirmation_number</span><span class="link-text" id="subHeaderTitle">Tickets</span></span>
             <ul class="nav-submenu" id="subMenu">
               <li class="nav-submenu-item"><a href="../pages/my_tickets.php" class="nav-link">My tickets</a></li>
               <li class="nav-submenu-item"><a href="../pages/create_ticket.php" class="nav-link">Create ticket</a></li>
-              <li class="nav-submenu-item"><a href="../pages/assigned_tickets.php" class="nav-link">Assigned tickets</a>
-              </li>
+              <li class="nav-submenu-item"><a href="../pages/assigned_tickets.php" class="nav-link">Assigned tickets</a></li>
+              <!--Same page: assigned and followed tickets (not assigned to them)-->
               <li class="nav-submenu-item"><a href="../pages/tickets.php" class="nav-link">All tickets</a></li>
             </ul>
           </span>
@@ -79,27 +83,26 @@ require_once(__DIR__ . '/../utils/session.php');
               class="material-symbols-outlined">quiz</span><span class="link-text">FAQs</span></a></li>
         <li class="nav-item"><a href="../actions/action_logout.php" class="nav-link"><span
               class="material-symbols-outlined">logout</span><span class="link-text">Log Out</span></a></li>
+          <!-- <span class="nav-logout"><span formaction="../actions/action_logout"
+              class="material-symbols-outlined">logout</span><span class="link-text">Log Out</span></span></li> -->
+        <!-- <li class="nav-item"><span class="nav-logout"><a href="../actions/action_logout" class="material-symbols-outlined">logout</span><span class = "link-text">Log Out</span></span></li> -->
       </ul>
 
     </nav>
-    <div class="nav-popup" id="navPopup">
-      <ul class="nav-popmenu" id="popMenu">
-        <li class="nav-submenu-item"><a href="../pages/my_tickets.php" class="nav-link">My tickets</a></li>
-        <li class="nav-submenu-item"><a href="../pages/create_ticket.php" class="nav-link">Create ticket</a></li>
-        <li class="nav-submenu-item"><a href="../pages/assigned_tickets.php" class="nav-link">Assigned tickets</a></li>
-        <li class="nav-submenu-item"><a href="../pages/tickets.php" class="nav-link">All tickets</a></li>
-      </ul>
-    </div>
+    <article></article>
+    <!--
     <section id="session-messages">
-      <article>
-      </article>
-    </section>
+        <article>
+        </article>
+      </section>
+    -->
+    <!-- <main id="page-container"> -->
     <main>
-    <?php } ?>
+<?php } ?>
 
 
 
-    <?php function output_footer()
+<?php function output_footer()
     { ?>
     </main>
     <footer>
@@ -109,7 +112,7 @@ require_once(__DIR__ . '/../utils/session.php');
 
   </html>
 <?php } ?>
-
+  
 <?php function drawLogoutForm( /* Session $session */)
 { ?>
   <form action="../actions/action_logout.php" method="post" class="logout">
