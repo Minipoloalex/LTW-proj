dbbce83b794edaf8f300781c4497822dd49cc11c
Félix Martins <felix.marcial@hotmail.com>
2023-05-13 21:44:32 +0100

    Fix on faq going to landing page if not logged in

--- pages/FAQ.php
+++ pages/FAQ.php
@@ -10,7 +10,7 @@ require_once(__DIR__ . '/../utils/session.php');
 
 $session = new Session();
 if (!$session->isLoggedIn()) {
-    header('Location: login.php');
+    header('Location: landing_page.php');
     exit();
 }
 
