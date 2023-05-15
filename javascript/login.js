b6bdae14575fd1fd7b77f9f9713518c5bf12f6d5
Sofia <up202108676@up.pt>
2023-05-15 19:06:25 +0100

    register show password

--- javascript/login.js
+++ javascript/login.js
@@ -1,4 +1,4 @@
-/*show password*/
+/*login*/
 
 const passwordInput = document.getElementById("password");
 const showPassButton = document.getElementById("showpass");
@@ -15,3 +15,24 @@ hidePassButton.addEventListener("click", function() {
   hidePassButton.hidden = true;
   showPassButton.hidden = false;
 });
+
+/*confirm password*/
+
+const confirmPasswordInput = document.getElementById("confirm-password");
+const showConfirmPassButton = document.getElementById("confirm-showpass");
+const hideConfirmPassButton = document.getElementById("confirm-hidepass");
+// console.log(showConfirmPassButton);
+
+if (showConfirmPassButton) {
+showConfirmPassButton.addEventListener("click", function() {
+    confirmPasswordInput.type = "text";
+    hideConfirmPassButton.hidden = false;
+    showConfirmPassButton.hidden = true;
+  });
+  
+hideConfirmPassButton.addEventListener("click", function() {
+    confirmPasswordInput.type = "password";
+    hideConfirmPassButton.hidden = true;
+    showConfirmPassButton.hidden = false;
+  });
+}
