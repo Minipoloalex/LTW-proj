4c2e2cf3e81e25f1d364aeb64f413b0496a0bdc1
Félix Martins <felix.marcial@hotmail.com>
2023-05-16 20:10:23 +0100

    Nav.js refactored to use classes (and nav.css)

--- javascript/nav.js
+++ javascript/nav.js
@@ -8,53 +8,28 @@ window.addEventListener("resize", function () {
 })
 
 subWrapper.addEventListener('click', function toggleSubMenu() {
-  const navbar = document.querySelector(".navbar");
-  // const navItems = document.querySelectorAll(".nav-item");
-  const subMenu = document.getElementById("subMenu");
   const navPopup = document.getElementById("navPopup");
 
-  // const subHeaderItems = document.querySelectorAll("#subWrapper .material-symbols-outlined, #subWrapper #subHeaderTitle");
-  const subHeader = document.querySelector(".nav-submenu-header");
-  console.log("click");
-
-  // if (window.matchMedia("(min-width: 600px)").matches) {
   if (window.matchMedia("(min-width: 961px)").matches) {
     //big screen
-    if (subMenu.style.display === "block") {
-      // para fechar
-      subMenu.style.display = "none";
-      subWrapper.style.height = "5rem";
-      subWrapper.style.alignItems = "center";
-      subHeader.style.paddingTop = "0rem";
+    if (subWrapper.classList.contains("active")) {
+      subWrapper.classList.remove("active");
     } else {
-      // para abrir
-      subMenu.style.display = "block";
-      subWrapper.style.height = "fit-content";
-      subWrapper.style.alignItems = "start";
-      subHeader.style.paddingTop = "2.05rem";
+      subWrapper.classList.add("active");
     }
 
     // Close submenu when mouse leaves navbar
     const navbar = document.querySelector(".navbar");
     navbar.addEventListener("mouseleave", function () {
-      // para fechar
-      subMenu.style.display = "none";
-      subWrapper.style.height = "5rem";
-      subWrapper.style.alignItems = "center";
-      subHeader.style.paddingTop = "0rem";
+      subWrapper.classList.remove("active");
     });
   }
   else {
     //small screen
-    if (navPopup.style.display === "block") {
+    if (navPopup.classList.contains("active")) {
       navPopup.classList.remove("active");
     } else {
       navPopup.classList.toggle("active");
-      // document.addEventListener('click', function(event) {
-      //     const outsideClick = (!navPopup.contains(event.target));
-      //     console.log(outsideClick);
-      //     if (outsideClick) { navPopup.classList.remove("active"); }
-      //   });
     }
   }
 });
